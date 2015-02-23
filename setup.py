792667ce6876b7e23594afd74104e68f1c631329
Çağıl <cagilulusahin@gmail.com>
2015-02-23 15:36:49 +0200

    Create setup.py
    author email missing

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Febrl',
+      version='0.4.2',
+      description='Febrl (Freely extensible biomedical record linkage)',
+      author='Peter Christen, 14 December 2011',
+      author_email='',
+      url='http://sourceforge.net/projects/febrl/',
+      packages=['febrl', ],
+     )
