29fe9f1c1642c815d114df8e7a2a5eee85a678ca
Çağıl <cagilulusahin@gmail.com>
2015-02-23 15:52:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+//from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='Febrl',
       version='0.4.2',
@@ -8,5 +9,5 @@ setup(name='Febrl',
       author='Peter Christen, 14 December 2011',
       author_email='',
       url='http://sourceforge.net/projects/febrl/',
-      #packages=['febrl', ],
+      packages=find_packages(exclude=['data', 'tests'])
      )
